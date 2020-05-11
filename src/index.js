c2868a47b7e3507a94320d545ccc4370e8eaa0a9
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-05-11 16:22:11 +0300

    Modified for IE

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import '@babel/polyfill'; //https://babeljs.io/docs/en/babel-polyfill
 import 'nodelist-foreach-polyfill'; //https://www.npmjs.com/package/nodelist-foreach-polyfill
-import elementClosest from'element-closest'; //https://www.npmjs.com/ С НИМ ВОТ ТАК!!!
+import elementClosest from 'element-closest'; //https://www.npmjs.com/ С НИМ ВОТ ТАК!!!
 elementClosest(window);
 import 'formdata-polyfill';
 import "regenerator-runtime/runtime.js";
@@ -9,7 +10,6 @@ import 'es6-promise';
 import 'fetch-polyfill';
 import 'whatwg-fetch';
 import 'scroll-behavior-polyfill';
-import '@babel/polyfill'; //https://babeljs.io/docs/en/babel-polyfill
 
 
 import countTimer from './modules/countTimer';
