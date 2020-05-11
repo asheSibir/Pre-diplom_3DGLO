bb9145164f1fd4e43b8b3aa5d29f84f6d9c9d26f
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-05-11 15:22:56 +0300

    Lets get it started! Once again

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,31 +1,32 @@
-const moveSlider =()=>{
-    const portfolio = document.getElementById('portfolio'),
-        slider = document.querySelector('.portfolio-content'), //id="all-progects">
-        slide = slider.querySelectorAll('.portfolio-item');
-    //ВОЗВРАЩЕНИЕ УДАЛЕННЫХ ДОТОВ
-        const dotsUl = document.createElement('ul');
-        dotsUl.classList.add('portfolio-dots');
-        slider.insertAdjacentElement('beforeend', dotsUl);
-        const dot = document.createElement('li');
-        dot.classList.add('dot');
-        dotsUl.appendChild(dot);
-        for (let i = 0; i < slide.length - 1; i++){
-            const newDot = dot.cloneNode(true);
-            dotsUl.appendChild(newDot);
-        }
-        const dots = dotsUl.querySelectorAll('li'); 
-        dots[0].classList.add('dot-active');
-
+// Slider
+const slider = () => {
+    const slider = document.querySelector('.portfolio-content'), //id="all-progects">
+        slide = slider.querySelectorAll('.portfolio-item'),
+        btn = slider.querySelectorAll('.portfolio-btn');
+//ВОЗВРАЩЕНИЕ УДАЛЕННЫХ ДОТОВ
+    const dotsUl = document.createElement('ul');
+    dotsUl.classList.add('portfolio-dots');
+    slider.insertAdjacentElement('beforeend', dotsUl);
+    const dot = document.createElement('li');
+    dot.classList.add('dot');
+    dotsUl.appendChild(dot);
+    for (let i = 0; i < slide.length - 1; i++){
+        const newDot = dot.cloneNode(true);
+        dotsUl.appendChild(newDot);
+    }
+    const dots = dotsUl.querySelectorAll('li'); 
+    dots[0].classList.add('dot-active');
+// ДОТЫ СНОВА С НАМИ            
     let currentSlide = 0, 
         interval; //для включения и остановки
-    
+
     const prevSlide = (elem, index, strClass) =>{
         elem[index].classList.remove(strClass);
     };
     const nextSlide = (elem, index, strClass) =>{
         elem[index].classList.add(strClass);
     };
-        // Сам слайдер, активация и дизактивация класса (видимости) элемента
+    // Сам слайдер, активация и дизактивация класса (видимости) элемента
     const autoPlaySlide = () => {
         prevSlide(slide, currentSlide, 'portfolio-item-active');
         prevSlide(dots, currentSlide, 'dot-active'); //ВЕРНУТЬ!!
@@ -42,19 +43,19 @@ const moveSlider =()=>{
     const stopSlide = () => {
         clearInterval(interval);
     };
-    
     slider.addEventListener('click', (event) =>{
         event.preventDefault();
         let target = event.target;
+
         if (target.matches('#arrow-right, #arrow-left, .dot')){
             prevSlide(slide, currentSlide, 'portfolio-item-active');
-            prevSlide(dots, currentSlide, 'dot-active'); 
+            prevSlide(dots, currentSlide, 'dot-active'); //ВЕРНУТЬ!!
             if (target.matches('#arrow-right')){
                 currentSlide++;
             } else if (target.matches('#arrow-left')){
                 currentSlide--;
                 } 
-                else if (target.matches('.dot')){ 
+                else if (target.matches('.dot')){ //ВЕРНУТЬ!!
                         dots.forEach((elem, index) => {
                             if (elem === target){
                                 currentSlide = index;
@@ -68,7 +69,7 @@ const moveSlider =()=>{
                 currentSlide = slide.length - 1;
             }
             nextSlide(slide, currentSlide, 'portfolio-item-active');
-            nextSlide(dots, currentSlide, 'dot-active'); 
+            nextSlide(dots, currentSlide, 'dot-active'); //ВЕРНУТЬ!!
         }
     });
     slider.addEventListener('mouseover', (event) => {
@@ -76,27 +77,12 @@ const moveSlider =()=>{
             stopSlide();
         }
     });
-     
-    startSlide(); 
-    portfolio.addEventListener('mouseenter', (event)=>{
-        console.log('mouseenter!');
-        startSlide();
-    });      
-    
-    portfolio.addEventListener('mouseleave', (event)=>{
-        stopSlide();
-        currentSlide = 0;
-        dots.forEach((dot) => {
-            dot.classList.remove('dot-active');
-        });
-        slide.forEach((slide) => {
-            slide.classList.remove('portfolio-item-active');
-        });
-        dots[0].classList.add('dot-active');
-        slide[0].classList.add('portfolio-item-active');
+    slider.addEventListener('mouseout', (event) => {
+        if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')){
+           startSlide(1500);
+        }
     });
-
-       
+    startSlide(1500);
+        
 };
-
-export default moveSlider;
+export default slider;
